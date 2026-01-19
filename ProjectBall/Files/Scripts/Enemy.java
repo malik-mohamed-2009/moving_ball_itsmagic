6cbdbe999bbc701f01f3e3ebaf16a32cdca4cc70
malik mohamed <malikmohamed11333@gmail.com>
2026-01-19 19:54:56 +0200

    Delete ProjectBall/Files/Scripts directory

--- ProjectBall/Files/Scripts/Enemy.java
+++
@@ -1,8 +0,0 @@
-void repeat() {
-    myObject.getPosition().setZ(myObject.getPosition().getZ() - Math.bySecond(5));
-    myObject.lookTo(WorldController.findObject("Player"));
-    
-    if (myObject.getPosition().getZ() < -10) {
-        myObject.destroy();
-    }
-}
